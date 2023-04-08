e0a41be6657b410afd34ba2dd622d986150c7a1e
imshani <shanirana6398@gmail.com>
2023-04-08 19:33:57 +0530

    First commit

---
+++ ATM.java
@@ -0,0 +1,21 @@
+package ATM;
+
+import java.io.IOException;
+public class ATM extends ChoiceMenu{
+	public static void main(String args[]) throws IOException
+	{
+	
+	ChoiceMenu choice=new ChoiceMenu();
+	choice.getLogin();
+}
+
+// Customer1:-
+
+// CustomerNumber--> 123123123 PinNumber--> 12345
+
+// Customer2:-
+
+// CustomerNumber--> 123412345 PinNumber--> 1236
+
+
+}
